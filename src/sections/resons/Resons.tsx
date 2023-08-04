d9fc519fe01bb24c98e1f3a4cafaa6500a0dca36
Andriikryl <andreyk557@gmail.com>
2023-08-04 15:18:26 +0300

    add reasons section component

--- src/sections/resons/Resons.tsx
+++ src/sections/resons/Resons.tsx
@@ -3,6 +3,27 @@ import React from "react";
 import style from "./style.module.css";
 import { VideoPlayer } from "@/components/videoPlayer";
 
+const data = [
+  {
+    id: 1,
+    title: "1. Поглиблена англійська",
+    description:
+      "Вивченню англійської приділяється особлива увага, адже сьогодні знання мови є необхідністю.",
+  },
+  {
+    id: 2,
+    title: "2. Вибір часу на предмет",
+    description:
+      "Ми можемо вибрати будь-яку кількість годин будь-якого предмета і зробити його таким чином основним",
+  },
+  {
+    id: 3,
+    title: "3. Маленькі класи",
+    description:
+      "Маленькі класи на 5-8 учнів дозволяють працювати персоналізовано з кожним учнем та приділяти достатньо уваги",
+  },
+];
+
 export function Resons() {
   return (
     <section className={style.resons}>
@@ -18,6 +39,16 @@ export function Resons() {
           </p>
         </div>
         <VideoPlayer />
+        <ul className={style.list}>
+          {data.map((item) => {
+            return (
+              <li key={item.id} className={style.list__item}>
+                <h4 className={style.item__title}>{item.title}</h4>
+                <p className={style.item__description}>{item.description}</p>
+              </li>
+            );
+          })}
+        </ul>
       </Container>
     </section>
   );
