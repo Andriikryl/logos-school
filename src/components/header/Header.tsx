8f2f1fffede73bdc4db811b7d1382253cd7beffa
Andriikryl <andreyk557@gmail.com>
2023-08-04 13:08:21 +0300

    add header component and button component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,61 @@
 import React from "react";
+import { Container } from "../container";
+import Image from "next/image";
+import Logo from "public/logo/logos-school 1.svg";
+import style from "./style.module.css";
+import { Button } from "../button/Button";
+
+const data = [
+  {
+    id: 1,
+    link: "Про школу",
+  },
+  {
+    id: 2,
+    link: "Школи",
+  },
+  {
+    id: 3,
+    link: "Переваги",
+  },
+  {
+    id: 4,
+    link: "Відгуки",
+  },
+  {
+    id: 5,
+    link: "Про нас",
+  },
+  {
+    id: 6,
+    link: "Контакти",
+  },
+];
 
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={style.header}>
+      <Container>
+        <div className={style.header__box}>
+          <a href="#">
+            <Image src={Logo} width={130} height={49} alt="logos school" />
+          </a>
+          <nav className={style.nav}>
+            <ul className={style.nav__list}>
+              {data.map((item) => {
+                return (
+                  <li key={item.id} className={style.list__item}>
+                    <a className={style.item__link} href="#">
+                      {item.link}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+          <Button>Записатися на урок</Button>
+        </div>
+      </Container>
+    </header>
+  );
 }
